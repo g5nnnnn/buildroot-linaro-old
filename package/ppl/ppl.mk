edb3f3eb7c0357d82d428bc8dea17d1d2f33d3cc
g5nnnnn <g6nnnnnn@gmail.com>
2012-08-30 10:23:18 +0800

    Bump up ppl version to 1.0

--- package/ppl/ppl.mk
+++ package/ppl/ppl.mk
@@ -4,7 +4,7 @@
 #
 #############################################################
 
-PPL_VERSION = 0.12.1
+PPL_VERSION = 1.0
 PPL_SITE = http://bugseng.com/products/ppl/download/ftp/releases/$(PPL_VERSION)
 PPL_SOURCE = ppl-$(PPL_VERSION).tar.bz2
 PPL_INSTALL_STAGING = YES
