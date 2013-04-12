5f9e49d6ef8a6220896c3452d40aa059956710db
g5nnnnn <g6nnnnnn@gmail.com>
2013-04-12 12:30:52 +0800

    linaro: add cloog 0.18 for gcc 4.8

--- package/cloog/cloog.mk
+++ package/cloog/cloog.mk
@@ -8,6 +8,9 @@ CLOOG_VERSION = 0.16.3
 ifeq ($(findstring x4.7.,x$(GCC_VERSION)),x4.7.)
 CLOOG_VERSION = 0.17.0
 endif
+ifeq ($(findstring x4.8.,x$(GCC_VERSION)),x4.8.)
+CLOOG_VERSION = 0.18.0
+endif
 CLOOG_SITE = http://www.bastoul.net/cloog/pages/download
 CLOOG_SOURCE = cloog-$(CLOOG_VERSION).tar.gz
 CLOOG_INSTALL_STAGING = YES
