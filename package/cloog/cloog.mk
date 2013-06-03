353736e68fd977a1da3f8a053e45ae35d78f8b06
g5nnnnn <g6nnnnnn@gmail.com>
2013-06-03 21:26:03 +0800

    cloog: add building dependencies

--- package/cloog/cloog.mk
+++ package/cloog/cloog.mk
@@ -5,16 +5,17 @@
 #############################################################
 
 CLOOG_VERSION = 0.16.3
+CLOOG_DEPENDENCIES = gmp ppl
 ifeq ($(findstring x4.7.,x$(GCC_VERSION)),x4.7.)
 CLOOG_VERSION = 0.17.0
 endif
 ifeq ($(findstring x4.8.,x$(GCC_VERSION)),x4.8.)
 CLOOG_VERSION = 0.18.0
+CLOOG_DEPENDENCIES = gmp isl
 endif
 CLOOG_SITE = http://www.bastoul.net/cloog/pages/download
 CLOOG_SOURCE = cloog-$(CLOOG_VERSION).tar.gz
 CLOOG_INSTALL_STAGING = YES
-CLOOG_DEPENDENCIES = gmp
 
 $(eval $(autotools-package))
 $(eval $(host-autotools-package))
